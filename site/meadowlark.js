23909ddead167654cab1aa395f7b7cc57dfd2f6c
OT Chen <ot32em@gmail.com>
2018-12-25 19:15:20 +0800

    temp ch05

--- site/meadowlark.js
+++ site/meadowlark.js
@@ -15,14 +15,23 @@ app.set('view engine', 'handlebars');
 
 // setup/local module: rolldice
 let rolldiceMod = require('./lib/rolldice.js')
-if(typeof rolldiceMode == 'undefined') {
+if(typeof rolldiceMod == 'undefined') {
 	console.error('rolldice can NOT be loaded');
 }
 
 
-
 // static
 app.use(expressMod.static(__dirname + '/public'));
+
+
+// route setup :: QA
+app.use(function(req, res, next) {
+	if( req.query.test === '1' && app.get('env') !== 'production')
+		res.locals.showTests = true;
+	else
+		res.locals.showTests = false;
+	next();
+})
 
 // route setup
 app.get('/', function(req, res) {
@@ -47,7 +56,7 @@ app.use(function(err, req, res, next) {
 
 app.listen(app.get('port'), function() {
 	console.log(
-		'Server(ch4-1) is up and listening port "'
+		'Server(ch5-1) is up and listening port "'
 		+ app.get('port')
 		+ '. Press ctrl+c to terminate server.');
 });
