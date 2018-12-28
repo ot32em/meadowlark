c984d4ef14834ea6ee6f1560b94caf7c31ff882a
OT Chen <ot32em@gmail.com>
2018-12-28 17:26:50 +0800

    make chrome options compact, try to silent the chrome msg 'Listening DevTools', 'UI lost GPU', blah blah but failed

--- site/qa/tests-crosspage.js
+++ site/qa/tests-crosspage.js
@@ -1,42 +1,35 @@
 let { Builder, By, Key, until } = require('selenium-webdriver');
 let chrome = require('selenium-webdriver/chrome');
-//let { suite, test} = require('mocha');
 let assert = require('chai').assert;
 
 
-let chromeOptions = new chrome.Options();
-chromeOptions.addArguments('--disable-gpu');
-chromeOptions.addArguments('--headless');
-
-// @type {WebDriver}
 let browser;
 
 suite('Crosspage Tests', function(){
     setup(function(){
          browser = new Builder()
                 .forBrowser('chrome')
-                .setChromeOptions(chromeOptions)
-                .build();
+                .setChromeOptions(new chrome.Options()
+                    .headless()
+                    .addArguments('--disable-gpu')
+                    .addArguments('--disable-extensions')
+                    .addArguments('--log-level=3')
+                ).build();
     });
 
     teardown(function(){
         browser.quit();
-        browser = undefined;
     });
 
     test('requesting group rate from hood river tour page should populate the referred field.',
         function(done){
             let referrer = 'http://localhost:3000/tours/hood-river';
             browser.get(referrer).then( _ => {
-                console.log('get(referrer)');
                 done();
                 return browser.findElement(By.className('requestGroupRate')).click();
             }).then( _ => {
-                console.log('findElement(.requestGroupRate)');
                 return browser.findElement(By.name('referrer')).getAttribute('value');
             }).then( referrerVal => {
-                console.log('findElement(.referrer)');
-                console.log(referrerVal);
                 assert(referrerVal === referrer);
                 done();
             }).catch( e => {});
