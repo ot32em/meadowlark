6f800ea0c9596278d90b29e57895b0813f2101e3
OT Chen <ot32em@gmail.com>
2019-01-03 10:18:38 +0800

    add lib/data.js

---
+++ site/lib/data.js
@@ -0,0 +1,27 @@
+exports.weather = {
+    "locations": [
+        {
+            "name": "台北",
+            "forecastUrl": "https://www.wunderground.com/weather/tw/taipei",
+            "iconUrl": "https://icons.wxug.com/i/c/v4/9.svg",
+            "weather": "多雲有雨",
+        },
+        {
+            "name": "台南",
+            "forecastUrl": "https://www.wunderground.com/weather/tw/tainan",
+            "iconUrl": "https://icons.wxug.com/i/c/v4/26.svg",
+            "weather": "多雲",
+        },
+        {
+            "name": "高雄市",
+            "forecastUrl": "https://www.wunderground.com/weather/tw/kaohsiung-city",
+            "iconUrl": "https://icons.wxug.com/i/c/v4/9.svg",
+            "weather": "多雲有雨",
+        },
+    ],
+};
+
+exports.tours = [
+    { id: 0, name: 'Hood River', price: 99.99},
+    { id: 1, name: 'Taipei', price: 149.99},
+];
