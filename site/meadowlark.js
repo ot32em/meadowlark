34db815c35f19fcce04349a9690a1e000f6211d4
OT Chen <ot32em@gmail.com>
2019-01-04 13:40:22 +0800

    add file upload

--- site/meadowlark.js
+++ site/meadowlark.js
@@ -198,7 +198,30 @@ app.post('/process', function(req, res) {
 	}
 });
 app.get('/thank-you', function(req, res) {
-	res.render('thank-you', {'msg': 'Email reigstered!'});
+	res.render('thank-you');
+});
+
+app.get('/contest/vacation-photo', function(req, res) {
+	let now = new Date();
+	res.render('contest/vacation-photo', {
+		'year': now.getFullYear(),
+		'month': now.getMonth(),
+	});
+});
+
+let formidable = require('formidable');
+app.post('/contest/vacation-photo/:year/:month', function(req, res) {
+	let form = new formidable.IncomingForm();
+	form.parse(req, function(err, fields, files) {
+		if(err) {
+			return res.redirect(303, '/error');
+		}
+		console.log('fields:');
+		console.log(fields);
+		console.log('files:');
+		console.log(files);
+		res.redirect(303, '/thank-you');
+	});
 });
 
 
