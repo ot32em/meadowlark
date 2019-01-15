18acaf20afcc86d9238fcaeb05613ffdb87d183f
OT Chen <ot32em@gmail.com>
2019-01-15 11:47:03 +0800

    adjust comment

--- app/meadowlark.js
+++ app/meadowlark.js
@@ -1,13 +1,15 @@
 let express = require('express');
 let app = express();
 
-// view
+
+// setup::basic app
 let handlebars = require('express3-handlebars').create({defaultLayout: 'main'});
 require('express-handlebars-sections')(handlebars);
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 
-// cookie
+
+// setup::middleware::server-level
 let secret = require('./config/confidential.js').secretCookie;
 app.use(require('cookie-parser')(secret));
 app.use(require('express-session')({
@@ -19,18 +21,20 @@ app.use(express.static(__dirname + '/public'));
 app.use(require('body-parser').urlencoded({'extended': true}));
 
 
+// setup::middleware::user
 app.use(require('./site/middlewares/auth'));
 app.use(require('./site/middlewares/flash'));
 app.use(require('./site/middlewares/qa'));
 app.use(require('./site/middlewares/weather'));
 
 
+// setup::routes
 app.use('/', require('./site/routes/root'));
 app.use('/tour', require('./site/routes/tour'));
 app.use('/api', require('./site/routes/api'));
 
 
-// route setup :: rest 404 and 500, routed to middleware
+// setup::error handling
 app.use(require('./site/middlewares/error').notFound);
 app.use(require('./site/middlewares/error').internalError);
 
