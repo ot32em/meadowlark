b7b75dd58bcd3a70507206d1828603cd36754be6
OT Chen <ot32em@gmail.com>
2019-01-15 09:52:28 +0800

    remove routes/index.js

--- app/site/routes/index.js
+++
@@ -1,9 +0,0 @@
-function init(app) {
-    app.use('/', require('./root'));
-    app.use('/tour/', require('./tour'));
-    app.use('/api/', require('./apis/'));
-}
-
-module.exports = {
-    'init': init,
-};
