82336fbc3b8796426e49d2c484b939c59b060a33
OT Chen <ot32em@gmail.com>
2019-01-16 10:23:07 +0800

    merge lib/nav.js into middleware/nav.js

--- app/middleware/nav.js
+++ app/middleware/nav.js
@@ -1,7 +1,33 @@
-let navLinks = require('../lib/nav');
+function navLinks() {
+    return [
+        { 'name': 'Home', 'link': "/",  },
+        { 'name': 'About', 'link': "/about", },
+        { 'name': 'Newsletter', 'link': "/newsletter", },
+        { 'name': 'Tours', 'links': [
+            { 'name': 'Tours', 'link': "/tour", },
+            { 'name': 'Hood River', 'link': "/tour/hood-river", },
+            { 'name': 'Taipei', 'link': "/tour/taipei", },
+            { 'name': 'Request Group Rate', 'link': "/tour/request-group-rate", },
+        ]},
+
+
+        { 'name': 'Photo Contest', 'links': [
+            { 'name': 'Photo Contest', 'link': "/contest/vacation-photo", },
+            { 'name': 'Photo Contest Ex', 'link': "/contest/vacation-photo-ex", },
+        ]},
+        
+        { 'name': 'Playground', 'links': [
+            { 'name': 'header', 'link': "/playground/header", },
+            { 'name': 'header-inspect', 'link': "/playground/header-inspect", },
+            { 'name': 'jquery-test', 'link': "/playground/jquerytest", },
+            { 'name': 'Nursery Rhyme', 'link': "/playground/nursery-rhyme", },
+            { 'name': 'cookie-test', 'link': "/playground/cookietest", },
+        ]},
+    ]
+}
 
 function nav(req, res, next) {
-    res.locals._nav = navLinks;
+    res.locals._nav = navLinks();
     next();
 }
 
