b5e280c4eefde952cc468439aa4ac4f6fad3817e
OT Chen <ot32em@gmail.com>
2019-01-16 10:29:11 +0800

    move routes unrelated files out from controllers/index.js to meadowlark.js

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,39 +1,11 @@
-let express = require('express');
+let app = new require('express').Router();
 
-function setup (app) {
-    // setup::middleware::server-level
-    let secret = require('../config/confidential.js').secretCookie;
-    app.use(require('cookie-parser')(secret));
-    app.use(require('express-session')({
-        'resave': false,
-        'saveUninitialized': false,
-        'secret':  secret,
-    }));
-    app.use(express.static(__dirname + '/../public'));
-    app.use(require('body-parser').urlencoded({'extended': true}));
+// setup::routes
+app.use('/', require('./site/'));
+app.use('/tour', require('./tour/'));
+app.use('/api/tour', require('./tour/api'));
+app.use('/playground', require('./playground/'));
+app.use('/api/playground', require('./playground/api'));
+app.use('/contest', require('./contest/'));
 
-
-    // setup::middleware::user
-    app.use(require('../middleware/nav'));
-    app.use(require('../middleware/auth'));
-    app.use(require('../middleware/flash'));
-    app.use(require('../middleware/qa'));
-    app.use(require('../middleware/weather'));
-
-
-    // setup::routes
-    app.use('/', require('./site/'));
-    app.use('/tour', require('./tour/'));
-    app.use('/api/tour', require('./tour/api'));
-    app.use('/playground', require('./playground/'));
-    app.use('/api/playground', require('./playground/api'));
-    app.use('/contest', require('./contest/'));
-
-    // setup::error handling
-    app.use(require('../middleware/error').notFound);
-    app.use(require('../middleware/error').internalError);
-}
-
-module.exports = {
-    'setup': setup        
-};
+module.exports = app;
