9d423db1cd6c2cef7be038d97c4f98583cc7c620
OT Chen <ot32em@gmail.com>
2019-01-24 15:31:21 +0800

    test

--- app/config/confidential.js
+++ app/config/confidential.js
@@ -1,21 +1,42 @@
 function readEnv(envKey) {
-    return process.env[envKey] || require('./confidential_env.js')[envKey];
+    console.log('typeof(process.env[envKey]): ' + typeof(process.env[envKey]));
+
+    let val;
+    if(typeof(process.env[envKey]) !== 'undefined') {
+        console.log('env key found ' + envKey);
+        val = process.env[envKey];
+    }
+    else {
+        console.log('env key not found ' + envKey);
+        val = require('./confidential_env')[envKey];
+
+    }
+    console.log('val: ' + val);
+    return val;
 }
 
+Object.keys(require('./confidential_env')).map(k => {
+    console.log('file, key: ' + k);
+});
+
+Object.keys(process.env).map(k => {
+    console.log('process env, key: ' + k);
+});
+
 module.exports = {
-    'secretCookie': readEnv('CONFIDENTIALS_SECRETCOOKIE'),
+    'secretCookie': readEnv('CONFIDENTIAL_SECRETCOOKIE'),
     'mailsender': {
-        'service': readEnv('CONFIDENTIALS_MAILSENDER_SERVICE'),
-        'email': readEnv('CONFIDENTIALS_MAILSENDER_EMAIL'),
-        'username': readEnv('CONFIDENTIALS_MAILSENDER_USERNAME'),
-        'password': readEnv('CONFIDENTIALS_MAILSENDER_PASSWORD'),
+        'service': readEnv('CONFIDENTIAL_MAILSENDER_SERVICE'),
+        'email': readEnv('CONFIDENTIAL_MAILSENDER_EMAIL'),
+        'username': readEnv('CONFIDENTIAL_MAILSENDER_USERNAME'),
+        'password': readEnv('CONFIDENTIAL_MAILSENDER_PASSWORD'),
     },
     'mongodb': {
         'development': {
-            'connectstring': readEnv('CONFIDENTIALS_MONGODB_DEVELOPMENT_CONNECTSTRING'), 
+            'connectstring': readEnv('CONFIDENTIAL_MONGODB_DEVELOPMENT_CONNECTSTRING'), 
         },
         'production': {
-            'connectstring': readEnv('CONFIDENTIALS_MONGODB_PRODUCTION_CONNECTSTRING'),
+            'connectstring': readEnv('CONFIDENTIAL_MONGODB_PRODUCTION_CONNECTSTRING'),
         }
     }
 };
